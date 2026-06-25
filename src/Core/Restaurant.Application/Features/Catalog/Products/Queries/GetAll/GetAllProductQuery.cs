e2085c0f121464cb23265bae945d118e98f181d7
Lain <chiyomiziwari@gmail.com>
2026-06-25 12:37:08 +0700

    sửa record

--- src/Core/Restaurant.Application/Features/Catalog/Products/Queries/GetAll/GetAllProductQuery.cs
+++ src/Core/Restaurant.Application/Features/Catalog/Products/Queries/GetAll/GetAllProductQuery.cs
@@ -1,9 +1,12 @@
-﻿using Restaurant.Application.Common.Abstraction;
+using Restaurant.Application.Common.Abstraction;
 using Restaurant.Application.Common.Models.Result;
 using Restaurant.Application.Messaging;
 using Restaurant.Contracts.DTOs.Catalog.Products;
 
 namespace Restaurant.Application.Features.Catalog.Products.Queries.GetAll
 {
-    public record GetAllProductQuery(string? CategoryName) : PageQuery, IQuery<PageResult<IEnumerable<ProductResponse>>>;
+    public record GetAllProductQuery : PageQuery, IQuery<PageResult<IEnumerable<ProductResponse>>>
+    {
+        public string? CategoryName { get; init; }
+    }
 }
