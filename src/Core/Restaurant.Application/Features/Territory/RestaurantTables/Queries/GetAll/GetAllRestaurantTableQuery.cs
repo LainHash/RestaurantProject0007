4fc619fd26298dc5f641ad2861cb43b7751f63a1
Lain <chiyomiziwari@gmail.com>
2026-06-26 12:09:15 +0700

    thêm api get all cho restaurant table

---
+++ src/Core/Restaurant.Application/Features/Territory/RestaurantTables/Queries/GetAll/GetAllRestaurantTableQuery.cs
@@ -0,0 +1,8 @@
+﻿using Restaurant.Application.Common.Models.Result;
+using Restaurant.Application.Messaging;
+using Restaurant.Contracts.DTOs.Territory.RestaurantTables;
+
+namespace Restaurant.Application.Features.Territory.RestaurantTables.Queries.GetAll
+{
+    public record GetAllRestaurantTableQuery : IQuery<DataResult<List<RestaurantTableResponse>>>;
+}
---
+++ src/External/Restaurant.Presentation/Controllers/Concrete/Territory/RestaurantTableController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant.Application.Features.Territory.Areas.Queries.GetAll;
+using Restaurant.Application.Features.Territory.RestaurantTables.Queries.GetAll;
+using Restaurant.Presentation.Controllers.Abstraction;
+
+namespace Restaurant.Presentation.Controllers.Concrete.Territory
+{
+    public class RestaurantTableController : ApiController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllRestaurantTableQuery query, CancellationToken cancellationToken)
+        {
+            var result = await Sender.Send(query, cancellationToken);
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
